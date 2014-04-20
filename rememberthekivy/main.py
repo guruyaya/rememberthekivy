eeb5642bb0563c18d8ca0b0d0f4ce00a290ba6fb
Yair Eshel <guruyaya@guruyaya-comp.(none)>
2014-04-20 08:36:52 +0300

    this is how cards look

--- rememberthekivy/main.py
+++ rememberthekivy/main.py
@@ -1,5 +1,5 @@
-
 from kivy.app import App
+from kivy.base import runTouchApp
 from kivy.lang import Builder
 from kivy.properties import ListProperty
 from kivy.uix.boxlayout import BoxLayout
@@ -8,24 +8,36 @@ from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 
 import time
 import random
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.uix.image import Image
 
-class MainMenuScreen(Screen):
+class PongGame(Widget):
     pass
 
-class GameScreen(Screen):
+class FirstScreen(Screen):
     pass
 
-class OptionsScreen(Screen):
+class SecondScreen(Screen):
+    pass
+
+class ColourScreen(Screen):
     colour = ListProperty([1., 0., 0., 1.])
 
 class MyScreenManager(ScreenManager):
     pass
-    
-# beggining defigning the app
-root_widget = Builder.load_file('rtk.pv')
 
-class ScreenManagerApp(App):
+class RtkApp(App):
     def build(self):
+        root_widget = Builder.load_file('rtk.kv')
+        pong_screen = root_widget.screens[0]
+        
+        card_list = [ list() for _ in range(5) ]
+        for i in range(5):
+            for j in range(5):
+                card_list[i].append( Image(source='colours.png', size_hint=[0.18,0.18], pos_hint={'top': (0.2 * (i+1)) - 0.01, 'right': (0.2 * (j+1)) - 0.01}) )
+                pong_screen.add_widget(card_list[i][j])
         return root_widget
 
-ScreenManagerApp().run()
+if __name__ == '__main__':
+    RtkApp().run()
