61e9ce37eed1a27fba32768623792bf5acf5a2dc
Yair Eshel <guruyaya@guruyaya-comp.(none)>
2014-04-20 21:21:09 +0300

    flex proprotions

--- rememberthekivy/main.py
+++ rememberthekivy/main.py
@@ -38,16 +38,28 @@ class Card(Image):
 
 class RtkApp(App):
     def build(self):
+        TOP_CARDS_SPACE = 0.3
+        ROWS = 4
+        COLS = 5
+        
+        # calculate the space of cards, and thier size
+        card_x_space = 1.0 / COLS
+        card_x_size = card_x_space - 0.01
+        
+        game_y_space = 1.0 - TOP_CARDS_SPACE
+        card_y_space = game_y_space / ROWS
+        card_y_size = card_y_space - 0.01
+        
         root_widget = Builder.load_file('rtk.kv')
         pong_screen = root_widget.screens[0]
         pong_screen.on_touch_up = img_touch
         
-        card_list = [ list() for _ in range(5) ]
-        for i in range(5):
-            for j in range(5):
+        card_list = [ list() for _ in range(ROWS) ]
+        for i in range(ROWS):
+            for j in range(COLS):
                 card_list[i].append( 
-                    Card(source='colours.png', size_hint=[0.18,0.18], 
-                          pos_hint={'top': (0.2 * (i+1)) - 0.01, 'right': (0.2 * (j+1)) - 0.01}))
+                    Card(source='colours.png', size_hint=[card_x_size, card_y_size], allow_stretch=True, keep_ratio=False,
+                          pos_hint={'top': (card_y_space * (i+1)), 'right': (card_x_space * (j+1)) - 0.005}))
                 pong_screen.add_widget(card_list[i][j])
                 
         return root_widget
