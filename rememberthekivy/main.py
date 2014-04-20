da5928290f67d19a601c8b005b01f60d253023b5
Yair Eshel <guruyaya@guruyaya-comp.(none)>
2014-04-20 23:28:40 +0300

    cards clickable

--- rememberthekivy/main.py
+++ rememberthekivy/main.py
@@ -16,14 +16,28 @@ from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.uix.image import Image
 
-def img_touch(touch_event):
-   pass 
 class PongGame(Widget):
     pass
 
 class FirstScreen(Screen):
-    pass
-
+    def img_touch(self, touch_event):
+        clickx, clicky = touch_event.spos
+        cardx, cardy = (0,0)
+        
+        # understand which col clicked
+        for i, position in enumerate(self.x_positions):
+            if clicky < position:
+                cardx = i
+                break
+        
+        # understand which row clicked
+        for i, position in enumerate(self.x_positions):
+            if clickx < position:
+                cardy = i
+                break
+            
+        self.card_list[cardx][cardy].source = 'colours2.png'
+        
 class SecondScreen(Screen):
     pass
 
@@ -34,33 +48,38 @@ class MyScreenManager(ScreenManager):
     pass
 
 class Card(Image):
-    pass
-
+    def __init__(self, **args):
+        Image.__init__(self, **args)
+        
 class RtkApp(App):
     def build(self):
         TOP_CARDS_SPACE = 0.3
         ROWS = 4
         COLS = 5
-        
+        CARDS_SPACING = 0.05
         # calculate the space of cards, and thier size
         card_x_space = 1.0 / COLS
-        card_x_size = card_x_space - 0.01
+        card_x_size = card_x_space - CARDS_SPACING
         
         game_y_space = 1.0 - TOP_CARDS_SPACE
         card_y_space = game_y_space / ROWS
-        card_y_size = card_y_space - 0.01
+        card_y_size = card_y_space - CARDS_SPACING
         
         root_widget = Builder.load_file('rtk.kv')
         pong_screen = root_widget.screens[0]
-        pong_screen.on_touch_up = img_touch
+        pong_screen.on_touch_up = pong_screen.img_touch
         
-        card_list = [ list() for _ in range(ROWS) ]
+        pong_screen.card_list = [ list() for _ in range(ROWS) ]
+        
+        # calculate expected positions of the cards
+        pong_screen.x_positions = [(card_x_space * (j+1)) - (CARDS_SPACING / 2) for j in range(COLS)]
+        pong_screen.y_positions = [(card_y_space * (i+1)) for i in range(ROWS)]
         for i in range(ROWS):
             for j in range(COLS):
-                card_list[i].append( 
+                pong_screen.card_list[i].append( 
                     Card(source='colours.png', size_hint=[card_x_size, card_y_size], allow_stretch=True, keep_ratio=False,
-                          pos_hint={'top': (card_y_space * (i+1)), 'right': (card_x_space * (j+1)) - 0.005}))
-                pong_screen.add_widget(card_list[i][j])
+                          pos_hint={'top': pong_screen.y_positions[i], 'right': pong_screen.x_positions[j]}))
+                pong_screen.add_widget(pong_screen.card_list[i][j])
                 
         return root_widget
 
