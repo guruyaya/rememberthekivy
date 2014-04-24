e2d21d33761944b5794e0ef98c95237e0486ccc8
Yair Eshel <guruyaya@guruyaya-comp.(none)>
2014-04-24 19:48:07 +0300

    well structured

--- rememberthekivy/main.py
+++ rememberthekivy/main.py
@@ -1,6 +1,6 @@
 ''' Kivy super advanced memory game. No change in hell I'm finishing everything in time
 '''
-__version__ = '0.1'
+__version__ = '0.2'
 
 from kivy.app import App
 from kivy.base import runTouchApp
@@ -24,7 +24,7 @@ DECK_LIST = [('Blue_Robot.png','Blue_Robot.png'), ('Garden_Witch_.png', 'Garden_
 
 BACK_SIDE = DECK_DIR + 'Waves_Pattern.png'
 
-TOP_CARDS_SPACE = 0
+TOP_CARDS_SPACE = 0.1
 ROWS = 4
 COLS = 5
 CARDS_SPACING = 0.02
@@ -70,30 +70,45 @@ def DeckHandler(deck_list, back_side):
     return cards
 
 class GameBoard:
-    def __init__(self, cards, rows, cols, card_spacing=0.2, top_card_space=0):
-        self.cards = cards
+    def __init__(self, canvas, deck, rows, cols, cards_spacing=0.2, top_cards_space=0):
+        self.canvas = canvas
+        self.deck = deck
         self.rows = rows
         self.cols = cols
-        self.card_spacing = card_spacing
-        self.top_card_space = top_card_space
+        self.cards_spacing = cards_spacing
+        self.top_cards_space = top_cards_space
         
         # preparing
         self.card_array = [list() for row in range(rows)]
     
-    def get_cards_cavas_positions():
+    def prepare_cards_cavas_positions(self):
         """ prepare the sizes and positions of all cards"""
         
         self.card_x_space = 1.0 / self.cols
-        self.card_x_size = card_x_space - self.card_spacing
+        self.card_x_size = self.card_x_space - self.cards_spacing
         
-        game_y_space = 1.0 - self.top_card_space
+        game_y_space = 1.0 - self.top_cards_space
         self.card_y_space = game_y_space / self.rows
-        self.card_y_size = card_y_space - self.card_spacing
+        self.card_y_size = self.card_y_space - self.cards_spacing
         
-class FirstScreen(Screen):
-    def on_touch_up(self, touch_event):
-        clickx, clicky = touch_event.spos
-        
+        # calculate expected positions of the cards
+        self.x_positions = [(self.card_x_space * (j+1)) - (self.cards_spacing / 2) for j in range(self.cols)]
+        self.y_positions = [(self.card_y_space * (i+1)) for i in range(self.rows)]
+    
+    def position_cards_on_canvas(self):
+        '''
+        '''
+        self.card_list = [ list() for _ in range(self.rows) ]
+        for i in range(self.rows):
+            for j in range(self.cols):
+                new_card = self.deck.pop()
+                new_card.pos_hint = {'top': self.y_positions[i], 'right': self.x_positions[j]}
+                self.card_list[i].append(new_card)
+                self.canvas.add_widget(new_card)
+                
+    def get_card_from_pos(self, clickx, clicky):
+        '''
+        '''
         # understand which col clicked
         for i, position in enumerate(self.x_positions):
             if clickx < position:
@@ -106,7 +121,12 @@ class FirstScreen(Screen):
                 cardy = i
                 break
         
-        self.card_list[cardy][cardx].flip_card()
+        return self.card_list[cardy][cardx]
+    
+class FirstScreen(Screen):
+    def on_touch_up(self, touch_event):
+        clickx, clicky = touch_event.spos
+        self.game_board.get_card_from_pos(clickx, clicky).flip_card()
         
 class SecondScreen(Screen):
     pass
@@ -117,30 +137,12 @@ class MyScreenManager(ScreenManager):
 class RtkApp(App):
     def build(self):
         deck = DeckHandler(deck_list=DECK_LIST, back_side=BACK_SIDE)
+        
         root_widget = Builder.load_file('rtk.kv')
-        pong_screen = root_widget.screens[0]
-
-        # calculate the space of cards, and thier size
-        card_x_space = 1.0 / COLS
-        card_x_size = card_x_space - CARDS_SPACING
-        
-        game_y_space = 1.0 - TOP_CARDS_SPACE
-        card_y_space = game_y_space / ROWS
-        card_y_size = card_y_space - CARDS_SPACING
-        
-        pong_screen.card_list = [ list() for _ in range(ROWS) ]
-        
-        # calculate expected positions of the cards
-        pong_screen.x_positions = [(card_x_space * (j+1)) - (CARDS_SPACING / 2) for j in range(COLS)]
-        pong_screen.y_positions = [(card_y_space * (i+1)) for i in range(ROWS)]
-        
-        for i in range(ROWS):
-            for j in range(COLS):
-                new_card = deck.pop()
-                new_card.pos_hint = {'top': pong_screen.y_positions[i], 'right': pong_screen.x_positions[j]}
-                pong_screen.card_list[i].append(new_card)
-                pong_screen.add_widget(pong_screen.card_list[i][j])
-                
+        game_screen = root_widget.screens[0]
+        game_screen.game_board = GameBoard(canvas=game_screen, deck=deck, rows=ROWS, cols=COLS, cards_spacing=CARDS_SPACING, top_cards_space=TOP_CARDS_SPACE)
+        game_screen.game_board.prepare_cards_cavas_positions()
+        game_screen.game_board.position_cards_on_canvas()               
         return root_widget
 
 if __name__ == '__main__':
